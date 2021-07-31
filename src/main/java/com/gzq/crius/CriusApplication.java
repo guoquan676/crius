caa70eecfa70ce4e0770a4b1cd0338fdf294965e
guozhenquan <guozhenquan@edianyun.com>
2021-07-31 11:56:45 +0800

    first commit

---
+++ src/main/java/com/gzq/crius/CriusApplication.java
@@ -0,0 +1,13 @@
+package com.gzq.crius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gzq/crius/CriusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzq.crius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
